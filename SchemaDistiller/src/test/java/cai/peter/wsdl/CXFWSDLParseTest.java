67bf3b72320d2f5f47608311983a58ccf94b69ff
Peter Cai <petercaica@hotmail.com>
2014-08-16 01:53:51 -0400

    2 parsers

--- SchemaDistiller/src/test/java/cai/peter/wsdl/CXFWSDLParseTest.java
+++ SchemaDistiller/src/test/java/cai/peter/wsdl/CXFWSDLParseTest.java
@@ -1,8 +1,12 @@
 package cai.peter.wsdl;
 
+import java.net.URL;
 import java.util.List;
 
 import javax.wsdl.Definition;
+import javax.wsdl.Message;
+import javax.wsdl.WSDLException;
+import javax.xml.namespace.QName;
 
 import org.apache.cxf.Bus;
 import org.apache.cxf.BusFactory;
@@ -13,29 +17,18 @@ import org.apache.cxf.wsdl11.WSDLServiceBuilder;
 import org.apache.ws.commons.schema.XmlSchema;
 import org.junit.Test;
 
-import com.predic8.schema.Schema;
-import com.predic8.wsdl.AbstractSOAPBinding;
-import com.predic8.wsdl.Binding;
-import com.predic8.wsdl.BindingOperation;
-import com.predic8.wsdl.Fault;
-import com.predic8.wsdl.Message;
-import com.predic8.wsdl.Operation;
-import com.predic8.wsdl.Part;
-import com.predic8.wsdl.Port;
-import com.predic8.wsdl.PortType;
-import com.predic8.wsdl.Service;
-
 public class CXFWSDLParseTest {
 
 	@Test
-	public void test() 
+	public void test() throws WSDLException 
 	{
+		URL wsdlUrl = this.getClass().getClassLoader().getResource("ebay/PayPalSvc.wsdl");
 
 		Bus bus = BusFactory.getDefaultBus();
 		WSDLManager wsdlManager = bus.getExtension(WSDLManager.class);
-		Definition definition = wsdlManager.getDefinition(wsdlUrl);
+		Definition defs = wsdlManager.getDefinition(wsdlUrl);
 		WSDLServiceBuilder wsdlServiceBuilder = new WSDLServiceBuilder(bus);
-		List<ServiceInfo> serviceInfos = wsdlServiceBuilder.buildServices(definition);
+		List<ServiceInfo> serviceInfos = wsdlServiceBuilder.buildServices(defs);
 		ServiceInfo serviceInfo = serviceInfos.get(0);
 		List<SchemaInfo> schemas = serviceInfo.getSchemas();
 		SchemaInfo schemaInfo = schemas.get(0);
@@ -44,21 +37,27 @@ public class CXFWSDLParseTest {
 
 		out("-------------- WSDL Details --------------");
 		out("TargenNamespace: \t" + defs.getTargetNamespace());
-		if (defs.getDocumentation() != null) {
-			out("Documentation: \t\t" + defs.getDocumentation());
-		}
+//		if (defs.getDocumentation() != null) {
+//			out("Documentation: \t\t" + defs.getDocumentation());
+//		}
 		out("\n");
 
 		/* For detailed schema information see the FullSchemaParser.java sample.*/
-		out("Schemas: ");
-		for (Schema schema : defs.getSchemas()) {
-			out("  TargetNamespace: \t" + schema.getTargetNamespace());
-		}
-		out("\n");
+//		out("Schemas: ");
+//		for (Schema schema : defs.getSchemas()) {
+//			out("  TargetNamespace: \t" + schema.getTargetNamespace());
+//		}
+//		out("\n");
 		
 		out("Messages: ");
-		for (Message msg : defs.getMessages()) {
-			out("  Message Name: " + msg.getName());
+//		Map messages = defs.getMessages();
+		for (Object key : defs.getMessages().keySet())
+		{
+			QName qName = (QName)key;
+			String localPart = qName.getLocalPart();
+			Message msg = defs.getMessage(qName);
+			out("");
+			out("  Message Name: " + localPart);
 			out("  Message Parts: ");
 			for (Part part : msg.getParts()) {
 				out("    Part Name: " + part.getName());
@@ -70,61 +69,61 @@ public class CXFWSDLParseTest {
 		out("");
 
 		out("PortTypes: ");
-		for (PortType pt : defs.getPortTypes()) {
-			out("  PortType Name: " + pt.getName());
-			out("  PortType Operations: ");
-			for (Operation op : pt.getOperations()) {
-				out("    Operation Name: " + op.getName());
-				out("    Operation Input Name: "
-				    + ((op.getInput().getName() != null) ? op.getInput().getName() : "not available!"));
-//				out("    Operation Input Message: "
-//				    + op.getInput().getMessage().getQname());
-				out("    Operation Output Name: "
-				    + ((op.getOutput().getName() != null) ? op.getOutput().getName() : "not available!"));
-//				out("    Operation Output Message: "
-//				    + op.getOutput().getMessage().getQname());
-				out("    Operation Faults: ");
-				if (op.getFaults().size() > 0) {
-					for (Fault fault : op.getFaults()) {
-						out("      Fault Name: " + fault.getName());
-//						out("      Fault Message: " + fault.getMessage().getQname());
-					}
-				} else out("      There are no faults available!");
-				
-			}
-			out("");
-		}
+//		for (PortType pt : defs.getPortTypes()) {
+//			out("  PortType Name: " + pt.getName());
+//			out("  PortType Operations: ");
+//			for (Operation op : pt.getOperations()) {
+//				out("    Operation Name: " + op.getName());
+//				out("    Operation Input Name: "
+//				    + ((op.getInput().getName() != null) ? op.getInput().getName() : "not available!"));
+////				out("    Operation Input Message: "
+////				    + op.getInput().getMessage().getQname());
+//				out("    Operation Output Name: "
+//				    + ((op.getOutput().getName() != null) ? op.getOutput().getName() : "not available!"));
+////				out("    Operation Output Message: "
+////				    + op.getOutput().getMessage().getQname());
+//				out("    Operation Faults: ");
+//				if (op.getFaults().size() > 0) {
+//					for (Fault fault : op.getFaults()) {
+//						out("      Fault Name: " + fault.getName());
+////						out("      Fault Message: " + fault.getMessage().getQname());
+//					}
+//				} else out("      There are no faults available!");
+//				
+//			}
+//			out("");
+//		}
 		out("");
 
 		out("Bindings: ");
-		for (Binding bnd : defs.getBindings()) {
-			out("  Binding Name: " + bnd.getName());
-			out("  Binding Type: " + bnd.getPortType().getName());
-			out("  Binding Protocol: " + bnd.getBinding().getProtocol());
-			out("  Binding Style: " + bnd.getStyle());
-			out("  Binding Operations: ");
-			for (BindingOperation bop : bnd.getOperations()) {
-				out("    Operation Name: " + bop.getName());
-				if(bnd.getBinding() instanceof AbstractSOAPBinding) {
-					out("    Operation SoapAction: " + bop.getOperation().getSoapAction());
-					out("    SOAP Body Use: " + bop.getInput().getBindingElements().get(0).getUse());
-				}
-			}
-			out("");
-		}
+//		for (Binding bnd : defs.getBindings()) {
+//			out("  Binding Name: " + bnd.getName());
+//			out("  Binding Type: " + bnd.getPortType().getName());
+//			out("  Binding Protocol: " + bnd.getBinding().getProtocol());
+//			out("  Binding Style: " + bnd.getStyle());
+//			out("  Binding Operations: ");
+//			for (BindingOperation bop : bnd.getOperations()) {
+//				out("    Operation Name: " + bop.getName());
+//				if(bnd.getBinding() instanceof AbstractSOAPBinding) {
+//					out("    Operation SoapAction: " + bop.getOperation().getSoapAction());
+//					out("    SOAP Body Use: " + bop.getInput().getBindingElements().get(0).getUse());
+//				}
+//			}
+//			out("");
+//		}
 		out("");
 
 		out("Services: ");
-		for (Service service : defs.getServices()) {
-			out("  Service Name: " + service.getName());
-			out("  Service Potrs: ");
-			for (Port port : service.getPorts()) {
-				out("    Port Name: " + port.getName());
-				out("    Port Binding: " + port.getBinding().getName());
-				out("    Port Address Location: " + port.getAddress().getLocation()
-				    + "\n");
-			}
-		}
+//		for (Service service : defs.getServices()) {
+//			out("  Service Name: " + service.getName());
+//			out("  Service Potrs: ");
+//			for (Port port : service.getPorts()) {
+//				out("    Port Name: " + port.getName());
+//				out("    Port Binding: " + port.getBinding().getName());
+//				out("    Port Address Location: " + port.getAddress().getLocation()
+//				    + "\n");
+//			}
+//		}
 		out("");
 	}
 
