217067716b7fade32392b6b64507304435b1b1d1
Peter Cai <petercaica@hotmail.com>
2014-08-24 02:25:28 -0400

    missed

---
+++ SchemaDistiller/src/main/java/cai/peter/schema/model/TypeInfo.java
@@ -0,0 +1,58 @@
+/***********************************************
+ * Copyright (c) 2014 Peter Cai                *
+ * All rights reserved.                        *
+ ***********************************************/
+package cai.peter.schema.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TypeInfo
+{
+	public TypeInfo()
+	{
+		super();
+		
+	}
+	public TypeInfo(String name)
+	{
+		super();
+		this.name = name;
+	}
+	String name;
+	String max, min;
+	List<String> enumeration = new ArrayList<String>();
+	public String getName()
+	{
+		return name;
+	}
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public String getMax()
+	{
+		return max;
+	}
+	public void setMax(String max)
+	{
+		this.max = max;
+	}
+	public String getMin()
+	{
+		return min;
+	}
+	public void setMin(String min)
+	{
+		this.min = min;
+	}
+	public List<String> getEnumeration()
+	{
+		return enumeration;
+	}
+	
+	public void addEnumeration(String val)
+	{
+		this.enumeration.add(val);
+	}
+}
